d3f318ca2dd00f24a74f2799abe3f736b07ad88a
Niranjna Kumar <niranjangiri51295@gmail.com>
2024-07-21 23:01:25 +0530

    Add Card Screen

--- src/components/FloatingPlayer.tsx
+++ src/components/FloatingPlayer.tsx
@@ -7,17 +7,22 @@ import { Track, useActiveTrack } from 'react-native-track-player'
 import { PlayPauseButton, SkipToNextButton } from './PlayerControls';
 import { useLastActiveTrack } from '@/hooks/useLastActiveTrack';
 import MovingText from './MovingText';
+import { useRouter } from 'expo-router';
 
 const FloatingPlayer = ({style}:ViewProps) => {
+    const router = useRouter();
+     
     const activeTrack = useActiveTrack();
     const lastActiveTrack = useLastActiveTrack()
 
     const displayerTrack = activeTrack ?? lastActiveTrack
     if(!displayerTrack) return null;
 
-   
+     const handlePress = () =>{
+        router.navigate('/player')
+     }
 
-    return <TouchableOpacity activeOpacity={0.9} style={[
+    return <TouchableOpacity onPress={handlePress} activeOpacity={0.9} style={[
         styles.container, style
     ]}>
         <>
